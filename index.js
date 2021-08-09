7767ce83875a5778fa0c8560c2a6c1b587e12200
Alan Race <alan@alanrace.com>
2021-08-09 15:24:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+import * as wasm from "wasm-test";
+
+var enc = new TextEncoder();
+
+var t0 = performance.now()
+let p = wasm.Parser.parse(enc.encode("<test></test>"));
+var t1 = performance.now()
+console.log("Parsing test took " + (t1 - t0) + " milliseconds.")
+console.log(p);
+while(p.errors()) {
+    console.log(p.get_next_error());
+}
+
+//var tickAddDot;
+
+document.querySelector("#read-file").addEventListener('click', function() {
+	// no file selected to read
+	if(document.querySelector("#file").value == '') {
+		console.log('No file selected');
+		return;
+	}
+
+	let validationResults = document.getElementById('validationResults');
+	validationResults.innerText = "Validating, please wait...";
+	/*tickAddDot = window.setInterval(() => {
+		validationResults.innerText += '.';
+	}, 100)*/
+
+	var file = document.querySelector("#file").files[0];
+
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		// binary data
+        var t0 = performance.now()
+
+        let array = new Uint8Array(e.target.result);
+        let p = wasm.Parser.parse(array);
+
+        var t1 = performance.now()
+
+        //console.log(p);
+		validationResults.innerHTML = '';
+        validationResults.innerText = "Parsing imzML took " + Math.round(t1 - t0) + " milliseconds.";
+		
+		let list = document.createElement('ul');
+		let numErrors = 0;
+		let maxErrors = 30;
+
+        while(p.errors()) {
+			if(numErrors < maxErrors) {
+				let item = document.createElement('li');
+				item.innerText = p.get_next_error();
+				list.appendChild(item);
+			}
+
+			numErrors += 1;
+        }
+
+		let errorText = document.createElement('span');
+		errorText.innerText = "Found " + numErrors + " errors.";
+		if(numErrors > maxErrors) {
+			errorText.innerText += " Displaying the first " + maxErrors + "."
+		}
+		validationResults.appendChild(errorText);
+
+		validationResults.appendChild(list);
+
+	};
+	reader.onerror = function(e) {
+		// error occurred
+		console.log('Error : ' + e.type);
+	};
+	
+	reader.readAsArrayBuffer(file);
+});
